54e9aae005d9f1e5b9173eb60762cc8ebcc05912
thaiphuc <thaihoangphuc98@gmail.com>
2024-03-14 08:26:08 +0700

    update wishlistpage

--- bookstore-client/src/pages/menuPage/FavoritePage.jsx
+++ bookstore-client/src/pages/menuPage/FavoritePage.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const FavoritePage = () => {
   return (
-      <div>      <h1 style={{ textAlign: 'center' }}>Yêu Thích</h1>
-</div>
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+      <div className={`bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100% `}>
+        <div className="py-28 flex flex-col items-center justify-center">
+          {/* content */}
+          <div className=" text-center px-4 space-y-7">
+            <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+              Your Favorite <span className="text-mainBG"> Books</span>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
-
 export default FavoritePage
