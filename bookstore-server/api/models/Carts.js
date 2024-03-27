a07c52c9e09075cc4542c293f78bcb71132a436b
PhamQui <20110079@student.hcmute.edu.vn>
2024-03-27 15:46:34 +0700

    update cart model

--- bookstore-server/api/models/Carts.js
+++ bookstore-server/api/models/Carts.js
@@ -3,13 +3,12 @@ const { Schema } = mongoose;
 
 const cartSchema = new Schema({
     bookItemId: String,
-    title: {
+    name: {
         type: String,
         trim: true,
         required: true,
         minlength: 3,
     },
-    recipe: String,
     image: String, 
     price: Number,
     quantity: Number,
