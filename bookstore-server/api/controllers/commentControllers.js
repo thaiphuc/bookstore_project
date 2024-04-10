b1b9d306c2411ab9793ef4f33b650c742e73ac6e
PhamQui <20110079@student.hcmute.edu.vn>
2024-04-10 15:05:13 +0700

    user commnet

--- bookstore-server/api/controllers/commentControllers.js
+++ bookstore-server/api/controllers/commentControllers.js
@@ -21,8 +21,39 @@ const deleteComment = async (req, res) => {
   }
 };
 
+//get cmt book
+const getAllComments = async (req, res) => {
+  const { bookId } = req.query; // Lấy bookId từ query parameter
+
+    try {
+        let query = {};
+
+        if (bookId) {
+            query.bookId = bookId;
+        }
+
+        const comments = await Comment.find(query);
+        res.json(comments);
+    } catch (error) {
+        res.status(500).send({ message: `Error getting comments: ${error.message}` });
+    }
+};
+
+
+// post a Comment item
+const postComment = async (req, res) => {
+  const newCmt = req.body;
+  try {
+    const result = await Comment.create(newCmt);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 
 module.exports = {
-  getAllComments,
   deleteComment,
+  postComment,
+  getAllComments
 };
