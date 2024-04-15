794cf31b261e7958ffe5c4402a2fa2e77a57c1d1
thaiphuc <thaihoangphuc98@gmail.com>
2024-04-15 15:45:57 +0700

    fix admin dashboard

--- bookstore-client/src/pages/dashboard/admin/Dashboard.jsx
+++ bookstore-client/src/pages/dashboard/admin/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
-import { FaBook, FaDollarSign, FaUsers } from "react-icons/fa";
+import { FaBook, FaDollarSign, FaShoppingBag, FaUsers } from "react-icons/fa";
 import {
   BarChart,
   Bar,
@@ -99,7 +99,7 @@ const Dashboard = () => {
       </h2>
       {/* stats */}
       <div className="stats shadow flex flex-col md:flex-row">
-        <div className="stat bg-emerald-200">
+        <div className="stat bg-cl1">
           <div className="stat-figure text-secondary">
             <FaDollarSign className="text-3xl"></FaDollarSign>
           </div>
@@ -108,7 +108,7 @@ const Dashboard = () => {
           <div className="stat-desc">Jan 1st - Feb 1st</div>
         </div>
 
-        <div className="stat bg-orange-200">
+        <div className="stat bg-cl2">
           <div className="stat-figure text-secondary">
             <FaUsers className="text-3xl"></FaUsers>
           </div>
@@ -117,7 +117,7 @@ const Dashboard = () => {
           <div className="stat-desc">↗︎ 400 (22%)</div>
         </div>
 
-        <div className="stat bg-indigo-400">
+        <div className="stat bg-cl3">
           <div className="stat-figure text-secondary">
             <FaBook className="text-3xl"></FaBook>
           </div>
@@ -126,21 +126,9 @@ const Dashboard = () => {
           <div className="stat-desc">↗︎ 400 (22%)</div>
         </div>
 
-        <div className="stat bg-purple-300">
+        <div className="stat bg-cl4">
           <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-8 h-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-              ></path>
-            </svg>
+            <FaShoppingBag className="text-3xl"></FaShoppingBag>
           </div>
           <div className="stat-title">Đơn hàng</div>
           <div className="stat-value">{stats.orders}</div>
